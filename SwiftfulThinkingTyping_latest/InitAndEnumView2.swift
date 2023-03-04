4ef89d92f425545c791ea6bdc26988bc5f81139e
SeoHaeChan <heroperson@naver.com>
2023-03-04 17:53:38 +0900

    Added new view

---
+++ SwiftfulThinkingTyping_latest/InitAndEnumView2.swift
@@ -0,0 +1,20 @@
+//
+//  InitAndEnumView2.swift
+//  SwiftfulThinkingTyping_latest
+//
+//  Created by root  on 2023/03/04.
+//
+
+import SwiftUI
+
+struct InitAndEnumView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitAndEnumView2_Previews: PreviewProvider {
+    static var previews: some View {
+        InitAndEnumView2()
+    }
+}
