0ff4c5020b76f93efff0cb6714ab99ea9f17ec3e
SeoHaeChan <heroperson@naver.com>
2023-03-04 17:58:17 +0900

    Added Text

--- SwiftfulThinkingTyping_latest/InitAndEnumView2.swift
+++ SwiftfulThinkingTyping_latest/InitAndEnumView2.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct InitAndEnumView2: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
