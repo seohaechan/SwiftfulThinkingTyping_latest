abcf37d5f381df20ea903a8172b6adb9dc0a61a7
Your Name Here <haechan.Dev@google.com>
2023-03-09 15:32:17 +0900

    peripheral changes

---
+++ SwiftfulThinkingTyping_latest/BindingView5.swift
@@ -0,0 +1,20 @@
+//
+//  BindingView5.swift
+//  SwiftfulThinkingTyping_latest
+//
+//  Created by root on 2023/03/08.
+//
+
+import SwiftUI
+
+struct BindingView5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingView5_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingView5()
+    }
+}
